5f59cfc0f0837fde25662a4fc6fff1544830569b
Chris McGlown <cmcglown@protonmail.com>
2026-01-14 20:33:05 -0500

    Adding workflow and test cases

---
+++ test_bad.sql
@@ -0,0 +1,5 @@
+-- Test case for SQL checker
+
+SELECT *
+FROM database.table
+WHERE insert_dt_utc > '2026-01-01';
---
+++ test_good.sql
@@ -0,0 +1,5 @@
+-- Test case for SQL checker
+
+SELECT *
+FROM table
+WHERE insert_dt_utc > '2026-01-01';
